69645939a26ffa0be7219ed202b0781c2b9a54a2
Krushna Chandra Nayak <krushnachandran33@gmail.com>
2026-03-20 23:19:48 +0530

    updated

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true,"Username is already taken"],
+        required:true
+    },
+    email:{
+        type:String,
+        unique:[true,"Email address is already taken by some other user"],
+        required:true
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+})
+
+const userModel = mongoose.model('User', userSchema)
+
+module.exports = userModel
