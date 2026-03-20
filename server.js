8709979b4dda8cc36814235f2c636e58dcbf9ee5
Krushna Chandra Nayak <krushnachandran33@gmail.com>
2026-03-20 23:44:57 +0530

    Remove all files except Backend folder

--- server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-const app = require('./src/app.js')
-const connectToDB = require('./src/config/database.js')
-
-connectToDB()
-
-app.listen(3000,()=>{
-    console.log("server is running on the port 3000")
-})
